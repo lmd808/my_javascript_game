59770b77581f4b7bf2108abcca8af92749d970ac
limabean <lditom808@gmail.com>
2019-09-08 20:07:21 -0400

    psudo

---
+++ assets/javascript/laura_mock_code.js
@@ -0,0 +1,48 @@
+// goal is to make a game where people can guess words by hitting keys on a keyboard
+// Ineed to log wins, losses and guess on my page
+// i would like to be able to reset the game each time after a win
+// maybe i'd like to figure out how to have the enter button reset the page
+
+//things I'll need for my game
+// make a variable for an array of words that I want people to guess
+
+// var words = ['an array'];
+
+// you can't guess or type an array so I will probably need to make the words in the away into strings
+
+// I need to make the words called random
+
+// i want to make blank spaces for the selected word on screen they'll be placeholder
+
+// i want to give my wins losses and guesses and guesses left  a value
+// it should probably be  # # [] #
+
+// game play function
+
+// pressing any key will start the game
+// a random word will be loaded from my words array
+// the word will go through a function that splits it up and then converts my little arrays into strings that can be called by my key strokes
+// the letters wont show in the blanks unless i click it so I'll make the new strings rest in the blanks
+// this will work because people can type strings and my blanks (i'll make a variable for them) will be set equal to a string. blanks + new strings = blanks
+
+// build something so that if the win
+
+// wins ++ x 1
+
+// if lose
+
+// losess ++ x 1
+// make a function for guesses used
+// if the guess is wrong the guesses-- x 1
+// we'll start with 9 guesses 10 in all
+
+// make a function for keystrokes
+// if the keystrokes don't match what's in the word put them into the players guesses string or array ... don't know what will work
+
+// check my game play accuracy (are the words right)
+//should i convert the strings back to an array to compae to the selected random word?
+// don't know yet
+
+// reset function
+
+// key up event handler
